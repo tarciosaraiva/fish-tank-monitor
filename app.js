79f5bb4a068f949ba2f03f46bf3b51c16a24a9c3
Tarcio <tarcio@gmail.com>
2014-03-16 20:29:52 +1100

    Improving logging

--- app.js
+++ app.js
@@ -12,12 +12,11 @@ var db = mongo.db(config.get('db:url'), {
 var ENV_FILE = 'TMP_FILE';
 var CFG_FILE = 'monitors:temperature:file';
 
-exports = module.exports = {
-    monitorFile: config.get(ENV_FILE) || config.get(CFG_FILE)
-};
+exports = module.exports = {};
 
 exports.process = function() {
     function dataReadCallback(data) {
+        logger.info('About to insert data: %s', data);
         db.collection('temperature').insert(data, function(err, result) {
             if (err !== null) {
                 logger.error('Could not insert temperature data: %s', err.message);
@@ -25,9 +24,11 @@ exports.process = function() {
         });
     };
 
+    var monitorFile = config.get(ENV_FILE) || config.get(CFG_FILE)
+
     every(config.get('poll')).do(function() {
-        logger.info('Polling file...');
-        reader.read(this.monitorFile, dataReadCallback);
+        logger.info('Polling file: %s', monitorFile);
+        reader.read(monitorFile, dataReadCallback);
     }, 5000);
 
 };
