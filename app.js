c78f7beb83da1d27faf1049c83e15703b36bf8a2
Tarcio <tarcio@gmail.com>
2014-03-18 16:53:50 +1100

    Added spec for app and modified some config and dependencies

--- app.js
+++ app.js
@@ -6,37 +6,37 @@ var every = require('schedule').every;
 
 // configure mongo
 var db = mongo.db(config.get('db:url'), {
-    native_parser: true
+  native_parser: true
 });
 
 var ENV_FILE = 'TMP_FILE';
 var ENV_POLL = 'TMP_POLL';
-var CFG_FILE = 'monitors:temperature:file';
+var CFG_FILE = 'monitors:temperature';
 var CFG_POLL = 'poll';
 
 exports = module.exports = {};
 
+exports.monitorFile = config.get(ENV_FILE) || config.get(CFG_FILE);
+exports.polling = config.get(ENV_POLL) || config.get(CFG_POLL);
+
 exports.process = function() {
-    var monitorFile = config.get(ENV_FILE) || config.get(CFG_FILE)
-    var polling = config.get(ENV_POLL) || config.get(CFG_POLL)
+  logger.info('Polling file `%s` every `%s`', this.monitorFile, this.polling);
 
-    logger.info('Polling file `%s` every `%s`', monitorFile, polling);
+  function dataReadCallback(data) {
+    if (typeof data === 'Error') {
+      logger.error('Error: %s', data.message);
+      process.exit(1);
+    }
 
-    function dataReadCallback(data) {
-        if (typeof data === 'Error') {
-            logger.error('Error: %s', data.message);
-            process.exit(1);
-        }
-        logger.info('About to insert data: %s', data);
-        db.collection('temperature').insert(data, function(err, result) {
-            if (err !== null) {
-                logger.error('Could not insert temperature data: %s', err.message);
-            }
-        });
-    };
+    db.collection('temperature').insert(data, function(err, result) {
+      if (err !== null) {
+        logger.error('Could not insert temperature data: %s', err.message);
+      }
+    });
+  };
 
-    every(polling).do(function() {
-        reader.read(monitorFile, dataReadCallback);
-    }, 5000);
+  every(this.polling).do(function() {
+    reader.read(this.monitorFile, dataReadCallback);
+  });
 
 };
---
+++ test/appSpec.js
@@ -0,0 +1,52 @@
+var expect = require('chai').expect
+var assert = require('chai').assert
+var should = require('should');
+var sinon = require('sinon');
+var reader = require('../lib/reader');
+var app = require('../app');
+
+describe('app', function() {
+
+  var clock, readerSpy = sinon.spy(reader, 'read');
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+  });
+
+  afterEach(function() {
+    clock.restore();
+  });
+
+  it('should read file according to config file', function() {
+    assert(app.monitorFile.should.be.eql('/tmp/content'));
+  });
+
+  it('should poll file according to env property', function() {
+    assert(app.polling.should.be.eql('5s'));
+  });
+
+  describe('#read()', function() {
+
+    it('should read file at correct intervals', function() {
+
+      // 31s
+      setTimeout(function() {
+        readerSpy.calledOn(reader);
+        readerSpy.callCount.should.be.eql(6);
+      }, 31000);
+
+      // 61s
+      setTimeout(function() {
+        readerSpy.calledOn(reader);
+        readerSpy.callCount.should.be.eql(12);
+      }, 61000);
+
+      app.process();
+
+      clock.tick(30000);
+      clock.tick(70000);
+    });
+
+  });
+
+});
