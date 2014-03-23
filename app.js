c2b4e9efd90709c3bc667eaeb24f3a1ee076d99e
Tarcio <tarcio@gmail.com>
2014-03-23 11:47:46 +1100

    Added .jshintrc and formatted some files according to rules

--- app.js
+++ app.js
@@ -19,24 +19,24 @@ exports = module.exports = {};
 exports.monitorFile = config.get(ENV_FILE) || config.get(CFG_FILE);
 exports.polling = config.get(ENV_POLL) || config.get(CFG_POLL);
 
-exports.process = function() {
+exports.process = function () {
   var self = this;
   logger.info('Polling file `%s` every `%s`', this.monitorFile, this.polling);
 
   function dataReadCallback(data) {
-    if (typeof data === 'Error') {
+    if (typeof data === Error) {
       logger.error('Error: %s', data.message);
       process.exit(1);
     }
 
-    db.collection('temperature').insert(data, function(err) {
+    db.collection('temperature').insert(data, function (err) {
       if (err !== null) {
         logger.error('Could not insert temperature data: %s', err.message);
       }
     });
   }
 
-  every(this.polling).do(function() {
+  every(this.polling).do(function () {
     reader.read(self.monitorFile, dataReadCallback);
   });
 
